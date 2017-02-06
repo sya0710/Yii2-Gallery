ddc31e578a2c10765936ffef026ba0b234dfed3a
Phongnv <phongnv093@gmail.com>
2017-02-06 09:40:03 +0700

    Add config message file, add message in translate

--- messages/vi/gallery.php
+++ messages/vi/gallery.php
@@ -2,4 +2,8 @@
 
 return [
     'Add Image' => 'Thêm ảnh mới',
+    'Image preview' => 'Hình ảnh',
+    'Image Infomaton' => 'Thông tin hình ảnh',
+    'Status' => 'Trạng thái',
+    'Action' => 'Hành động'
 ];
