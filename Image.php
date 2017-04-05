94c30371fa6cb26c3fcd216a6176aa85d04bbdd3
Phongnv <phongnv093@gmail.com>
2017-04-05 14:51:57 +0700

    Update upload single image

---
+++ Image.php
@@ -0,0 +1,197 @@
+<?php
+namespace sya\gallery;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use sya\gallery\helpers\FileHelper;
+use yii\helpers\Json;
+use yii\helpers\Url;
+use yii\web\View;
+
+class Image extends \sya\gallery\widgets\BaseWidget {
+    
+    public $layouts = <<< HTML
+        <div id="tableImage" style="margin-bottom: 10px;">
+            {infomationImage}
+        </div>
+HTML;
+
+    public $infomationImage = <<< HTML
+        {image}
+        {typeImage}
+        {infomation}
+        {columns}
+HTML;
+
+    public $infomationImageDetail = <<< HTML
+        {fieldInput}
+HTML;
+
+    /**
+     * @param $template String template gallery
+     * @throws \Exception
+     */
+    protected function renderLayout($template){
+        echo $template;
+        $this->renderModal();
+    }
+
+    /**
+     * Ham tao giao dien cho image
+     * @param array $galleries mang cac gia tri cua image
+     * @param array $columns danh sach truong cua image
+     * @param string $module Module name
+     * @param string $attribute Attribute name
+     * @param string $template Template infomation image
+     * @param string $templateInfomationDetail Template infomation detail image
+     * @return string
+     */
+    public static function generateGalleryTemplate($galleries, $columns = [], $module, $attribute, $template = null, $templateInfomationDetail = null){
+        $templateGallery = '';
+
+        if (empty($galleries) OR !is_array($galleries))
+            return null;
+        
+        foreach ($galleries as $galleryId => $gallery) {
+            // Tao giao dien cho cac image
+            $templateGallery .= self::buildTemplateRow($gallery, $galleryId, $module, $attribute, $columns, $template, $templateInfomationDetail);
+        }
+        return $templateGallery;
+    }
+
+    protected function buildTemplateRow($gallery, $galleryId, $module, $attribute, $columns, $template, $templateInfomationDetail){
+        // Gia tri mac dinh cua image
+        $urlImg = ArrayHelper::getValue($gallery, 'url');
+        $type = ArrayHelper::getValue($gallery, 'type');
+        $title = ArrayHelper::getValue($gallery, 'title');
+        $caption = ArrayHelper::getValue($gallery, 'caption');
+        $alt_text = ArrayHelper::getValue($gallery, 'alt_text');
+        $options = ArrayHelper::getValue($gallery, 'options', []);
+        $optionsDefault = ['class' => 'text-center', 'style' => 'vertical-align: middle;'];
+        $options = ArrayHelper::merge($optionsDefault, $options);
+        // Build layout image
+        $layouts = $template;
+        $replace = [];
+        if (strpos($layouts, '{image}') !== false) {
+            $imageTemplate = self::generateImageByType($urlImg, $type);
+            $replace['{image}'] = $imageTemplate;
+        }
+        if (strpos($layouts, '{typeImage}') !== false) {
+            $typeImageTemplate = Html::hiddenInput($module . '[' . $attribute . '][' . $galleryId . '][type]', $type, ['class' => 'form-control']);
+            $replace['{typeImage}'] = $typeImageTemplate;
+        }
+        if (strpos($layouts, '{infomation}') !== false) {
+            $infomationTemplate = self::renderInfomationImage($galleryId, $urlImg, $title, $caption, $alt_text, $module, $attribute, $templateInfomationDetail);
+            $replace['{infomation}'] = $infomationTemplate;
+        }
+        if (strpos($layouts, '{columns}') !== false) {
+            $columnsTemplate = self::renderColumnsImage($columns, $gallery, $galleryId, $options, $module, $attribute);
+            $replace['{columns}'] = $columnsTemplate;
+        }
+        if (strpos($layouts, '{actions}') !== false) {
+            $actionsTemplate = Html::button('<i class="fa fa-trash"></i>', ['class' => 'btn btn-white', 'onclick' => 'syaremoveImage(this);']);
+            $replace['{actions}'] = $actionsTemplate;
+        }
+        return strtr($layouts, $replace);
+    }
+
+    protected static function generateImageByType($urlImg, $type){
+        $options = ['style' => 'max-height: 300px;', 'class' => 'img-responsive'];
+        $template = null;
+        switch ($type) {
+            case self::TYPE_URL:
+                $template = Html::img($urlImg, $options);
+                break;
+            default:
+                $template = Html::img(FileHelper::getFileUploaded($urlImg), $options);
+                break;
+        }
+        return $template;
+    }
+
+    protected function renderInfomationImage($galleryId, $urlImg, $title, $caption, $alt_text, $module, $attribute, $templateInfomationDetail){
+        $template = null;
+        // config layout infomation image
+        $layouts = $templateInfomationDetail;
+        $replace = [];
+        // Html attribute default input
+        $options = [
+            'class' => 'form-control'
+        ];
+        // Array column infomation for
+        $infomation = [
+            'url' => [
+                'label' => 'Url',
+                'value' => $urlImg,
+                'options' => ArrayHelper::merge([
+                    'readonly' => true
+                ], $options)
+            ],
+            'title' => [
+                'label' => 'Title',
+                'value' => $title,
+                'options' => $options
+            ],
+            'caption' => [
+                'label' => 'Caption',
+                'value' => $caption,
+                'options' => $options
+            ],
+            'alt_text' => [
+                'label' => 'Alt text',
+                'value' => $alt_text,
+                'options' => $options
+            ]
+        ];
+        foreach ($infomation as $nameColumns => $info) {
+            $label = ArrayHelper::getValue($info, 'label');
+            $value = ArrayHelper::getValue($info, 'value');
+            $options = ArrayHelper::getValue($info, 'options');
+            if (strpos($layouts, '{title}') !== false) {
+                $titleTemplate = $label;
+                $replace['{title}'] = $titleTemplate;
+            }
+            if (strpos($layouts, '{fieldInput}') !== false) {
+                $fileTemplate = Html::hiddenInput($module . '[' . $attribute . '][' . $galleryId . '][' . $nameColumns . ']', $value, $options);
+                $replace['{fieldInput}'] = $fileTemplate;
+            }
+            $template .= strtr($layouts, $replace);
+        }
+        return $template;
+    }
+
+    protected function registerAssets(){
+        \sya\gallery\GalleryAssets::register($this->getView());
+        parent::registerAssets();
+
+        $this->getView()->registerJs('
+            function addImageByGallery(type){
+                var module = "' . $this->moduleName . '",
+                    attribute = "' . $this->attribute . '",
+                    templateInfomationImage = \'' . str_replace(array("\r\n", "\n", "\r"), '', $this->infomationImage) . '\',
+                    templateInfomationImageDetail = \'' . str_replace(array("\r\n", "\n", "\r"), '', $this->infomationImageDetail) . '\',
+                    columns = \'' . Json::encode($this->columns) . '\',
+                    image = $("#image").val(),
+                    widget_class = "/sya/gallery/Image";
+                if (image.length > 0) {
+                    $.ajax({
+                        url: "' . Url::to(['/gallery/ajax/additemimage']) . '",
+                        type: "post",
+                        data: {type: type, widget_class: widget_class, module: module, attribute: attribute, columns: columns, image: image, templateInfomationImage: templateInfomationImage, templateInfomationImageDetail: templateInfomationImageDetail},
+                    }).done(function (data) {
+                        if (data.length > 0) {
+                            $("' . $this->syaContainer . '").html("");
+                            $("' . $this->syaContainer . '").append(data);
+                            $("#sya_gallery_modal").modal("hide");
+                            // Reset value
+                            $("#sya_gallery_path").find(".active").removeClass("active");
+                            $("#sya_gallery_viewpath").html("");
+                            $(".sya_image_input").val("");
+                        }
+                    });
+                }
+            }
+        ', View::POS_END);
+    }
+}
--- controllers/AjaxController.php
+++ controllers/AjaxController.php
@@ -2,7 +2,6 @@
 
 namespace sya\gallery\controllers;
 
-use sya\gallery\Gallery;
 use Yii;
 use sya\gallery\helpers\FileHelper;
 use yii\helpers\ArrayHelper;
@@ -18,6 +17,10 @@ class AjaxController extends \yii\web\Controller{
         // Cac thong so mac dinh cua image
         // Kieu upload
         $type = Yii::$app->request->post('type');
+        
+        // Widget call function build image
+        $widget_class = Yii::$app->request->post('widget_class', '/sya/gallery/Gallery');
+        $widget_class = $this->rewriteWidgetClass($widget_class);
 
         // Module upload
         $module = Yii::$app->request->post('module');
@@ -46,7 +49,7 @@ class AjaxController extends \yii\web\Controller{
         $id = uniqid('g_');
 
         // Begin upload image
-        if ($type == Gallery::TYPE_UPLOAD){// Upload anh khi chon type la upload
+        if ($type == $widget_class::TYPE_UPLOAD){// Upload anh khi chon type la upload
             $images = UploadedFile::getInstancesByName('image');
             if (empty($images))
                 return;
@@ -72,8 +75,8 @@ class AjaxController extends \yii\web\Controller{
                 }
             }
             
-            $template = Gallery::generateGalleryTemplateByPath($gallery);
-        } elseif ($type == Gallery::TYPE_URL) {// Lay ra duong dan anh khi type la url
+            $template = $widget_class::generateGalleryTemplateByPath($gallery);
+        } elseif ($type == $widget_class::TYPE_URL) {// Lay ra duong dan anh khi type la url
             $image = Yii::$app->request->post('image');
 
             $columnsDefault = Json::decode($image);
@@ -85,8 +88,8 @@ class AjaxController extends \yii\web\Controller{
                 'type' => $type
             ], $columnsDefault);
 
-            $template = Gallery::generateGalleryTemplate($gallery, $columns, $module, $attribute, $templateInfomationImage, $templateInfomationImageDetail);
-        } elseif ($type == Gallery::TYPE_PATH) {
+            $template = $widget_class::generateGalleryTemplate($gallery, $columns, $module, $attribute, $templateInfomationImage, $templateInfomationImageDetail);
+        } elseif ($type == $widget_class::TYPE_PATH) {
             $image = Yii::$app->request->post('image');
 
             if (empty($image))
@@ -105,10 +108,9 @@ class AjaxController extends \yii\web\Controller{
                 }
             }
 
-            $template = Gallery::generateGalleryTemplate($gallery, $columns, $module, $attribute, $templateInfomationImage, $templateInfomationImageDetail);
+            $template = $widget_class::generateGalleryTemplate($gallery, $columns, $module, $attribute, $templateInfomationImage, $templateInfomationImageDetail);
         }
         // End upload image
-        
         echo $template;
     }
 
@@ -116,20 +118,28 @@ class AjaxController extends \yii\web\Controller{
         // Image
         $image = Yii::$app->request->post('image');
 
+        // Widget call function build image
+        $widget_class = Yii::$app->request->post('widget_class', '/sya/gallery/Gallery');
+        $widget_class = $this->rewriteWidgetClass($widget_class);
+
         if (empty($image))
             return;
 
-        echo Gallery::generateInsertFromUrl($image);
+        echo $widget_class::generateInsertFromUrl($image);
     }
 
     public function actionGetinfoimage() {
         // Image
         $image = Yii::$app->request->post('image');
 
+        // Widget call function build image
+        $widget_class = Yii::$app->request->post('widget_class', '/sya/gallery/Gallery');
+        $widget_class = $this->rewriteWidgetClass($widget_class);
+
         if (empty($image))
             return;
 
-        echo Gallery::generatePreviewImage($image);
+        echo $widget_class::generatePreviewImage($image);
     }
 
     /**
@@ -140,6 +150,14 @@ class AjaxController extends \yii\web\Controller{
 
         $page = Yii::$app->request->post('page', 1);
 
-        echo Gallery::getGalleryByPath($path, $page);
+        // Widget call function build image
+        $widget_class = Yii::$app->request->post('widget_class', '/sya/gallery/Gallery');
+        $widget_class = $this->rewriteWidgetClass($widget_class);
+
+        echo $widget_class::getGalleryByPath($path, $page);
+    }
+
+    function rewriteWidgetClass($widget_class) {
+        return str_replace('/', '\\', $widget_class);
     }
 }
